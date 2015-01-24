2ec4e637dd03906213e6c74e3d90025b210b08b4
mdanilov <mdanilov@3detection.ru>
2015-01-24 20:57:15 +0300

    add glContextFactory

--- com/watabou/noosa/Game.java
+++ com/watabou/noosa/Game.java
@@ -23,6 +23,7 @@ import java.util.TreeMap;
 
 import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.egl.EGLContext;
 import javax.microedition.khronos.egl.EGLDisplay;
 import javax.microedition.khronos.opengles.GL10;
 
@@ -42,6 +43,7 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.media.AudioManager;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.GLSurfaceView.EGLContextFactory;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.util.DisplayMetrics;
@@ -51,20 +53,21 @@ import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.View;
 
-public class Game extends Activity implements GLSurfaceView.Renderer, View.OnTouchListener {
+public class Game extends Activity implements GLSurfaceView.Renderer,
+		View.OnTouchListener {
 
 	public static Game instance;
 	private static Context context;
-	
+
 	// Actual size of the screen
 	public static int width;
 	public static int height;
-	
+
 	// Density: mdpi=1, hdpi=1.5, xhdpi=2...
 	public static float density = 1;
-	
+
 	public static String version;
-	
+
 	// Current scene
 	protected Scene scene;
 	// New scene we are going to switch to
@@ -73,198 +76,197 @@ public class Game extends Activity implements GLSurfaceView.Renderer, View.OnTou
 	protected boolean requestedReset = true;
 	// New scene class
 	protected Class<? extends Scene> sceneClass;
-	
+
 	// Current time in milliseconds
 	protected long now;
-	// Milliseconds passed since previous update 
+	// Milliseconds passed since previous update
 	protected long step;
-	
+
 	public static float timeScale = 1f;
 	public static float elapsed = 0f;
-	
+
 	protected GLSurfaceView view;
 	protected SurfaceHolder holder;
-	
+
 	// Accumulated touch events
 	protected ArrayList<MotionEvent> motionEvents = new ArrayList<MotionEvent>();
-	
+
 	// Accumulated key events
 	protected ArrayList<KeyEvent> keysEvents = new ArrayList<KeyEvent>();
-	
-	public Game( Class<? extends Scene> c ) {
+
+	public Game(Class<? extends Scene> c) {
 		super();
 		sceneClass = c;
 	}
-	
+
 	@Override
-	protected void onCreate( Bundle savedInstanceState ) {
-		super.onCreate( savedInstanceState );
-		
-		//Criado para manter o contexto e poder fazer a busca dos resources
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Criado para manter o contexto e poder fazer a busca dos resources
 		context = getApplicationContext();
-		
+
 		BitmapCache.context = TextureCache.context = instance = this;
-		
+
 		DisplayMetrics m = new DisplayMetrics();
-		getWindowManager().getDefaultDisplay().getMetrics( m );
+		getWindowManager().getDefaultDisplay().getMetrics(m);
 		density = m.density;
-		
+
 		try {
-			version = getPackageManager().getPackageInfo( getPackageName(), 0 ).versionName;
+			version = getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
 		} catch (NameNotFoundException e) {
 			version = "???";
 		}
-		
-		setVolumeControlStream( AudioManager.STREAM_MUSIC );
-		
-		view = new GLSurfaceView( this );
-		view.setEGLContextClientVersion( 2 );
+
+		setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
+		view = new GLSurfaceView(this);
+		// view.setEGLContextClientVersion( 2 );
+		view.setEGLContextFactory(new glContextFactory());
 		view.setEGLConfigChooser(new glConfigChoser());
-		view.setRenderer( this );
-		view.setOnTouchListener( this );
-		setContentView( view );
+		view.setRenderer(this);
+		view.setOnTouchListener(this);
+		setContentView(view);
 	}
-	
+
 	@Override
 	public void onResume() {
 		super.onResume();
-		
+
 		now = 0;
 		view.onResume();
-		
+
 		Music.INSTANCE.resume();
 		Sample.INSTANCE.resume();
 	}
-	
+
 	@Override
 	public void onPause() {
 		super.onPause();
-		
+
 		if (scene != null) {
 			scene.pause();
 		}
-		
+
 		view.onPause();
 		Script.reset();
-		
+
 		Music.INSTANCE.pause();
 		Sample.INSTANCE.pause();
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
 		destroyGame();
-		
+
 		Music.INSTANCE.mute();
 		Sample.INSTANCE.reset();
 	}
 
 	@SuppressLint({ "Recycle", "ClickableViewAccessibility" })
 	@Override
-	public boolean onTouch( View view, MotionEvent event ) {
+	public boolean onTouch(View view, MotionEvent event) {
 		synchronized (motionEvents) {
-			motionEvents.add( MotionEvent.obtain( event ) );
+			motionEvents.add(MotionEvent.obtain(event));
 		}
 		return true;
 	}
-	
+
 	@Override
-	public boolean onKeyDown( int keyCode, KeyEvent event ) {
-		
-		if (keyCode == Keys.VOLUME_DOWN || 
-			keyCode == Keys.VOLUME_UP) {
-			
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+		if (keyCode == Keys.VOLUME_DOWN || keyCode == Keys.VOLUME_UP) {
+
 			return false;
 		}
-		
+
 		synchronized (motionEvents) {
-			keysEvents.add( event );
+			keysEvents.add(event);
 		}
 		return true;
 	}
-	
+
 	@Override
-	public boolean onKeyUp( int keyCode, KeyEvent event ) {
-		
-		if (keyCode == Keys.VOLUME_DOWN || 
-			keyCode == Keys.VOLUME_UP) {
-			
+	public boolean onKeyUp(int keyCode, KeyEvent event) {
+
+		if (keyCode == Keys.VOLUME_DOWN || keyCode == Keys.VOLUME_UP) {
+
 			return false;
 		}
-		
+
 		synchronized (motionEvents) {
-			keysEvents.add( event );
+			keysEvents.add(event);
 		}
 		return true;
 	}
-	
+
 	@Override
-	public void onDrawFrame( GL10 gl ) {
-		
+	public void onDrawFrame(GL10 gl) {
+
 		if (width == 0 || height == 0) {
 			return;
 		}
-		
+
 		SystemTime.tick();
 		long rightNow = SystemTime.now;
 		step = (now == 0 ? 0 : rightNow - now);
 		now = rightNow;
-		
+
 		step();
 
 		NoosaScript.get().resetCamera();
-		GLES20.glScissor( 0, 0, width, height );
-		GLES20.glClear( GLES20.GL_COLOR_BUFFER_BIT );
+		GLES20.glScissor(0, 0, width, height);
+		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 		draw();
 	}
 
 	@Override
-	public void onSurfaceChanged( GL10 gl, int width, int height ) {
-		
-		GLES20.glViewport( 0, 0, width, height );
-		
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+		GLES20.glViewport(0, 0, width, height);
+
 		Game.width = width;
 		Game.height = height;
 
 	}
 
 	@Override
-	public void onSurfaceCreated( GL10 gl, EGLConfig config ) {
-		GLES20.glEnable( GL10.GL_BLEND );
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		GLES20.glEnable(GL10.GL_BLEND);
 		// For premultiplied alpha:
 		// GLES20.glBlendFunc( GL10.GL_ONE, GL10.GL_ONE_MINUS_SRC_ALPHA );
-		GLES20.glBlendFunc( GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA );
-		
-		GLES20.glEnable( GL10.GL_SCISSOR_TEST );
-		
+		GLES20.glBlendFunc(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+
+		GLES20.glEnable(GL10.GL_SCISSOR_TEST);
+
 		TextureCache.reload();
 	}
-	
+
 	protected void destroyGame() {
 		if (scene != null) {
 			scene.destroy();
 			scene = null;
 		}
-		
+
 		instance = null;
 	}
-	
+
 	public static void resetScene() {
-		switchScene( instance.sceneClass );
+		switchScene(instance.sceneClass);
 	}
-	
-	public static void switchScene( Class<? extends Scene> c ) {
+
+	public static void switchScene(Class<? extends Scene> c) {
 		instance.sceneClass = c;
 		instance.requestedReset = true;
 	}
-	
+
 	public static Scene scene() {
 		return instance.scene;
 	}
-	
+
 	protected void step() {
-		
+
 		if (requestedReset) {
 			requestedReset = false;
 			try {
@@ -274,58 +276,59 @@ public class Game extends Activity implements GLSurfaceView.Renderer, View.OnTou
 				e.printStackTrace();
 			}
 		}
-		
+
 		update();
 	}
-	
+
 	protected void draw() {
 		scene.draw();
 	}
-	
+
 	protected void switchScene() {
 
 		Camera.reset();
-		
+
 		if (scene != null) {
 			scene.destroy();
 		}
 		scene = requestedScene;
 		scene.create();
-		
+
 		Game.elapsed = 0f;
 		Game.timeScale = 1f;
 	}
-	
+
 	protected void update() {
 		Game.elapsed = Game.timeScale * step * 0.001f;
-		
+
 		synchronized (motionEvents) {
-			Touchscreen.processTouchEvents( motionEvents );
+			Touchscreen.processTouchEvents(motionEvents);
 			motionEvents.clear();
 		}
 		synchronized (keysEvents) {
-			Keys.processTouchEvents( keysEvents );
+			Keys.processTouchEvents(keysEvents);
 			keysEvents.clear();
 		}
-		
-		scene.update();		
+
+		scene.update();
 		Camera.updateAll();
 	}
-	
-	public static void vibrate( int milliseconds ) {
-		((Vibrator)instance.getSystemService( VIBRATOR_SERVICE )).vibrate( milliseconds );
+
+	public static void vibrate(int milliseconds) {
+		((Vibrator) instance.getSystemService(VIBRATOR_SERVICE))
+				.vibrate(milliseconds);
 	}
-	
-	public static String getVar(int id){
+
+	public static String getVar(int id) {
 		return context.getResources().getString(id);
 	}
-	public static String[] getVars(int id){
+
+	public static String[] getVars(int id) {
 		return context.getResources().getStringArray(id);
 	}
 }
 
-class glConfigChoser implements android.opengl.GLSurfaceView.EGLConfigChooser
-{
+class glConfigChoser implements android.opengl.GLSurfaceView.EGLConfigChooser {
 
 	@Override
 	public EGLConfig chooseConfig(EGL10 egl, EGLDisplay display) {
@@ -344,73 +347,96 @@ class glConfigChoser implements android.opengl.GLSurfaceView.EGLConfigChooser
 		}
 	}
 
-	private int getAttribute(EGL10 egl, EGLDisplay display,EGLConfig config, int attribute){
-		int[] value = {0};
+	private int getAttribute(EGL10 egl, EGLDisplay display, EGLConfig config,
+			int attribute) {
+		int[] value = { 0 };
 
 		egl.eglGetConfigAttrib(display, config, attribute, value);
 		return value[0];
 	}
-	
-	private int formatScore(int r, int g, int b, int a, int d, int s)
-	{
+
+	private int formatScore(int r, int g, int b, int a, int d, int s) {
 		int score = 0;
-		
-		if(r+g+b == 16){
+
+		if (r + g + b == 16) {
 			score += 2;
 		}
-		
-		if(r+g+b > 16){
+
+		if (r + g + b > 16) {
 			score += 1;
 		}
-		
-		if(d > 0){
+
+		if (d > 0) {
 			score -= 1;
 		}
-		
-		if(d > 16){
+
+		if (d > 16) {
 			score -= 1;
 		}
-		
-		if(a > 0){
-			score -=1;
+
+		if (a > 0) {
+			score -= 1;
 		}
-		
-		if(s > 0){
-			score -=1;
+
+		if (s > 0) {
+			score -= 1;
 		}
-		
+
 		return score;
 	}
-	
-	private EGLConfig chooseConfig(EGL10 egl, EGLDisplay display,EGLConfig configList[]) throws Exception {
-		
-		TreeMap<Integer,Integer> formats = new TreeMap<Integer,Integer>();
-		
-		Log.i("glConfigChoser",String.format("config num: %d", configList.length));
+
+	private EGLConfig chooseConfig(EGL10 egl, EGLDisplay display,
+			EGLConfig configList[]) throws Exception {
+
+		TreeMap<Integer, Integer> formats = new TreeMap<Integer, Integer>();
+
+		Log.i("glConfigChoser",
+				String.format("config num: %d", configList.length));
 		for (int i = 0; i < configList.length; i++) {
 			EGLConfig config = configList[i];
 
-			int r =  getAttribute(egl,display,config,EGL10.EGL_RED_SIZE);
-			int b =  getAttribute(egl,display,config,EGL10.EGL_BLUE_SIZE);
-			int g =  getAttribute(egl,display,config,EGL10.EGL_GREEN_SIZE);
-			
-			int a = getAttribute(egl,display,config,EGL10.EGL_ALPHA_SIZE);
-			int d = getAttribute(egl,display,config,EGL10.EGL_DEPTH_SIZE);
-			
-			int s = getAttribute(egl,display,config,EGL10.EGL_STENCIL_SIZE);
-		
-			formats.put(formatScore(r,g,b,a,d,s),i);
-			
-			Log.i("glConfigChoser",String.format("%d -> r: %d, g: %d, b: %d, a: %d, d: %d, s: %d",i,r,g,b,a,d,s));				
+			int r = getAttribute(egl, display, config, EGL10.EGL_RED_SIZE);
+			int b = getAttribute(egl, display, config, EGL10.EGL_BLUE_SIZE);
+			int g = getAttribute(egl, display, config, EGL10.EGL_GREEN_SIZE);
+
+			int a = getAttribute(egl, display, config, EGL10.EGL_ALPHA_SIZE);
+			int d = getAttribute(egl, display, config, EGL10.EGL_DEPTH_SIZE);
+
+			int s = getAttribute(egl, display, config, EGL10.EGL_STENCIL_SIZE);
+
+			formats.put(formatScore(r, g, b, a, d, s), i);
+
+			Log.i("glConfigChoser", String.format(
+					"%d -> r: %d, g: %d, b: %d, a: %d, d: %d, s: %d",
+					i, r, g, b, a, d, s));
 		}
-	
-		Entry<Integer,Integer> best = formats.lastEntry();
-		
+
+		Entry<Integer, Integer> best = formats.lastEntry();
+
 		EGLConfig config = configList[best.getValue()];
-		
-		Log.i("glConfigChoser",String.format("chosen: %d",best.getValue()));
-		
+
+		Log.i("glConfigChoser", String.format("chosen: %d", best.getValue()));
+
 		return config;
 	}
-	
+}
+
+class glContextFactory implements EGLContextFactory {
+	private int EGL_CONTEXT_CLIENT_VERSION = 0x3098;
+
+	public EGLContext createContext(EGL10 egl, EGLDisplay display,
+			EGLConfig config) {
+		int[] attrib_list = { EGL_CONTEXT_CLIENT_VERSION, 2, EGL10.EGL_NONE };
+
+		return egl.eglCreateContext(display, config, EGL10.EGL_NO_CONTEXT,
+				attrib_list);
+	}
+
+	public void destroyContext(EGL10 egl, EGLDisplay display,
+			EGLContext context) {
+		if (!egl.eglDestroyContext(display, context)) {
+			Log.e("DefaultContextFactory", "display:" + display
+					+ " context: " + context);
+		}
+	}
 }
